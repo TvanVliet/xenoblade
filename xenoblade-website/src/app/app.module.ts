46465a17e569670785927b59aea5919b6d920d3a
TvanVliet <dev.timvanvliet@gmail.com>
2018-11-21 21:24:25 +0100

    Add footer and article component

--- xenoblade-website/src/app/app.module.ts
+++ xenoblade-website/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
